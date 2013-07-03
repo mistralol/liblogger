cdba3da6b9f875e18862a4cc8d597169ffdd7264
James Stevenson <james@stev.org>
2013-07-03 16:32:50 +0100

    Expanded syslog to support all log levels

--- src/LogSyslog.cpp
+++ src/LogSyslog.cpp
@@ -27,10 +27,23 @@ void LogSyslog::Log(LogType Type, const std::string str)
 		case LOGGER_INFO:
 			syslog(LOG_INFO, "%s", str.c_str());			
 			break;
+		case LOGGER_NOTICE:
+			syslog(LOG_NOTICE, "%s", str.c_str());
+			break;
+		case LOGGER_WARNING:
+			syslog(LOG_WARNING, "%s", str.c_str());
+			break;
 		case LOGGER_ERR:
 			syslog(LOG_ERR, "%s", str.c_str());
+			break;
+		case LOGGER_CRIT:
+			syslog(LOG_CRIT, "%s", str.c_str());
+			break;
 		case LOGGER_ALERT:
 			syslog(LOG_ALERT, "%s", str.c_str());
+			break;
+		case LOGGER_EMERG:
+			syslog(LOG_EMERG, "%s", str.c_str());
 			break;
 		default:
 			abort();
