2ef9360af9dbfc38979f1c74d792fa4b4b13806d
James Stevenson <james@stev.org>
2013-07-16 10:38:18 +0100

    Added Log Functions

--- include/liblogger/Logger.h
+++ include/liblogger/Logger.h
@@ -8,6 +8,24 @@ namespace liblogger
 	extern void Logger(const char *fmt, ...);
 	extern void Logger(const std::string &str);
 
+	extern void LogDebug(const char *fmt, ...);
+	extern void LogDebug(const std::string &str);
+	extern void LogInfo(const char *fmt, ...);
+	extern void LogInfo(const std::string &str);
+	extern void LogNotice(const char *fmt, ...);
+	extern void LogNotice(const std::string &str);
+	extern void LogWarning(const char *fmt, ...);
+	extern void LogWarning(const std::string &str);
+	extern void LogError(const char *fmt, ...);
+	extern void LogError(const std::string &str);
+	extern void LogCritical(const char *fmt, ...);
+	extern void LogCritical(const std::string &str);
+	extern void LogAlert(const char *fmt, ...);
+	extern void LogAlert(const std::string &str);
+	extern void LogEmergency(const char *fmt, ...);
+	extern void LogEmergency(const std::string &str);
+
+
 	/* Ask for log rotation */
 	extern void LoggerRotate();
 };
--- src/Logger.cpp
+++ src/Logger.cpp
@@ -47,6 +47,166 @@ void Logger(const std::string &str)
 	Logger(LOGGER_INFO, str);
 }
 
+void LogDebug(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_DEBUG, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogDebug(const std::string &str)
+{
+	LogDebug("%s", str.c_str());
+}
+
+void LogInfo(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_INFO, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogInfo(const std::string &str)
+{
+	LogInfo("%s", str.c_str());
+}
+
+void LogNotice(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_NOTICE, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogNotice(const std::string &str)
+{
+	LogNotice("%s", str.c_str());
+}
+
+void LogWarning(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_WARNING, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogWarning(const std::string &str)
+{
+	LogWarning("%s", str.c_str());
+}
+
+void LogError(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_ERR, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogError(const std::string &str)
+{
+	LogError("%s", str.c_str());
+}
+
+void LogCritical(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_CRIT, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogCritical(const std::string &str)
+{
+	LogCritical("%s", str.c_str());
+}
+
+void LogAlert(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_ALERT, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogAlert(const std::string &str)
+{
+	LogAlert("%s", str.c_str());
+}
+
+void LogEmergency(const char *fmt, ...)
+{
+	char *tmp = 0;
+	
+    va_list ap;
+    va_start(ap, fmt);
+	int res = vasprintf(&tmp, fmt, ap);
+	if (res < 0)
+		abort();
+	std::string str = tmp;
+	Logger(LOGGER_EMERG, str);
+	free(tmp);
+    va_end(ap);
+}
+
+void LogEmergency(const std::string &str)
+{
+	LogEmergency("%s", str.c_str());
+}
+
 void LoggerRotate()
 {
 	LogManager::Rotate();
