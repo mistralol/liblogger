148c4d0f8c5bf7feecc0ec9b35f193f592fbddc9
James Stevenson <james@stev.org>
2013-07-16 15:13:07 +0100

    Improved Formatting with date / pid / thread id / sequence number etc..

--- include/liblogger/LogManager.h
+++ include/liblogger/LogManager.h
@@ -18,10 +18,43 @@ namespace liblogger
 			static void Lock();
 			static void Unlock();
 
+			static bool GetAddSeq();
+			static void SetAddSeq(bool bEnabled);
+
+			static bool GetAddDate();
+			static void SetAddDate(bool bEnabled);
+
+			static std::string GetDateFormat();
+			static void SetDateFormat(const std::string &str);
+
+			static bool GetAddProcessName();
+			static void SetAddProcessName(bool bEnabled);
+
+			static std::string GetProcessName();
+			static void SetProcessName(const std::string &str);
+
+			static bool GetAddProcessID();
+			static void SetAddProcessID(bool bEnabled);
+
+			static bool GetAddThreadID();
+			static void SetAddThreadID(bool bEnabled);
+
+			static bool GetAddSeperator();
+			static void SetAddSeperator(bool bEnabled);
+
 		private:
 			static std::list<ILogger *> m_loggers;
 			static pthread_mutex_t m_mutex;
 			static bool m_locked;
+			static std::string m_DateFormat;
+			static std::string m_ProcessName;
+
+			static bool m_AddSeq;
+			static bool m_AddDate;
+			static bool m_AddProcessName;
+			static bool m_AddProcessID;
+			static bool m_AddThreadID;
+			static bool m_AddSeperator;
 
 			static uint64_t m_TotalMessages;
 	};
--- src/LogManager.cpp
+++ src/LogManager.cpp
@@ -12,6 +12,15 @@ std::list<ILogger *> LogManager::m_loggers;
 pthread_mutex_t LogManager::m_mutex;
 bool LogManager::m_locked = false;
 uint64_t LogManager::m_TotalMessages = 0;
+std::string LogManager::m_DateFormat = "%F %T";
+std::string LogManager::m_ProcessName = "";
+
+bool LogManager::m_AddSeq = true;
+bool LogManager::m_AddDate = true;
+bool LogManager::m_AddProcessName = true;
+bool LogManager::m_AddProcessID = true;
+bool LogManager::m_AddThreadID = false;
+bool LogManager::m_AddSeperator = true;
 
 void LogManager::Init()
 {
@@ -45,7 +54,46 @@ void LogManager::Send(const LogType Type, const std::string &str)
 	Lock();
 	m_TotalMessages++;	
 	for( std::list<ILogger *>::iterator i = m_loggers.begin(); i != m_loggers.end(); i++)
-		(*i)->Log(Type, str);
+	{
+		std::stringstream ss;
+
+		if (m_AddDate && (*i)->AddDate())
+		{
+			time_t current;
+			struct tm timeinfo;
+			char buf[128];
+			time(&current);
+			localtime_r(&current, &timeinfo);
+			strftime(buf, sizeof(buf), m_DateFormat.c_str(), &timeinfo);
+			ss << buf << " ";
+		}
+
+		if (m_AddSeq && (*i)->AddSeq())
+			ss << "<" << m_TotalMessages << "> ";
+
+		if (m_AddProcessName && (*i)->AddProcessName())
+		{
+			ss << m_ProcessName << " ";
+		}
+
+		if (m_AddProcessID && (*i)->AddProcessID())
+		{
+			ss << "[PID:" << getpid() << "] ";
+		}
+
+		if (m_AddThreadID && (*i)->AddThreadID())
+		{
+			ss << "[TID:" << pthread_self() << "] ";
+		}
+
+		if (m_AddSeperator && (*i)->AddSeperator())
+		{
+			ss << "- ";
+		}
+
+		ss << str;
+		(*i)->Log(Type, ss.str());
+	}
 	Unlock();
 }
 
@@ -94,6 +142,86 @@ void LogManager::Unlock()
 	m_locked = false;
 	if (pthread_mutex_unlock(&m_mutex) < 0)
 		abort();
+}
+
+bool LogManager::GetAddSeq()
+{
+	return m_AddSeq;
+}
+
+void LogManager::SetAddSeq(bool bEnabled)
+{
+	m_AddSeq = bEnabled;
+}
+
+bool LogManager::GetAddDate()
+{
+	return m_AddDate;
+}
+
+void LogManager::SetAddDate(bool bEnabled)
+{
+	m_AddDate = bEnabled;
+}
+
+std::string LogManager::GetDateFormat()
+{
+	return m_DateFormat;
+}
+
+void LogManager::SetDateFormat(const std::string &str)
+{
+	m_DateFormat = str;
+}
+
+bool LogManager::GetAddProcessName()
+{
+	return m_AddProcessName;
+}
+
+void LogManager::SetAddProcessName(bool bEnabled)
+{
+	m_AddProcessName = bEnabled;
+}
+
+std::string LogManager::GetProcessName()
+{
+	return m_ProcessName;
+}
+
+void LogManager::SetProcessName(const std::string &str)
+{
+	m_ProcessName = str;
+}
+
+bool LogManager::GetAddProcessID()
+{
+	return m_AddProcessID;
+}
+
+void LogManager::SetAddProcessID(bool bEnabled)
+{
+	m_AddProcessID = bEnabled;
+}
+
+bool LogManager::GetAddThreadID()
+{
+	return m_AddThreadID;
+}
+
+void LogManager::SetAddThreadID(bool bEnabled)
+{
+	m_AddThreadID = bEnabled;
+}
+
+bool LogManager::GetAddSeperator()
+{
+	return m_AddSeperator;
+}
+
+void LogManager::SetAddSeperator(bool bEnabled)
+{
+	m_AddSeperator = bEnabled;
 }
 
 };
