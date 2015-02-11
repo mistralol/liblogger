3575cd75f402cce82e8e7472d3bd13a1bf835b18
James Stevenson <james@stev.org>
2015-02-11 16:29:40 +0000

    Backout last change

--- include/liblogger/ILogger.h
+++ include/liblogger/ILogger.h
@@ -11,7 +11,7 @@ namespace liblogger
 			virtual void GetName(std::string *str) { };
 			virtual void GetDesc(std::string *str) { };
 
-			virtual void Rotate() { Log(LOGGER_INFO, "LogRorate"); };
+			virtual void Rotate() { };
 			virtual void Log(const LogType Type, const std::string &str) { };
 
 			virtual bool AddSeq() { return true; }
