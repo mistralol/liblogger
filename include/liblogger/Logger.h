2571f7716efd24676a7ea0c34d8decc42ca300f9
James Stevenson <james@stev.org>
2015-03-16 13:38:33 +0000

    Added GCC attribute flags to log functions

--- include/liblogger/Logger.h
+++ include/liblogger/Logger.h
@@ -9,21 +9,21 @@ namespace liblogger
 	extern void Logger(const std::string &str);
 
 	/* Cleaner Aliases for the above */
-	extern void LogDebug(const char *fmt, ...);
+	extern void LogDebug(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogDebug(const std::string &str);
-	extern void LogInfo(const char *fmt, ...);
+	extern void LogInfo(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogInfo(const std::string &str);
-	extern void LogNotice(const char *fmt, ...);
+	extern void LogNotice(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogNotice(const std::string &str);
-	extern void LogWarning(const char *fmt, ...);
+	extern void LogWarning(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogWarning(const std::string &str);
-	extern void LogError(const char *fmt, ...);
+	extern void LogError(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogError(const std::string &str);
-	extern void LogCritical(const char *fmt, ...);
+	extern void LogCritical(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogCritical(const std::string &str);
-	extern void LogAlert(const char *fmt, ...);
+	extern void LogAlert(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogAlert(const std::string &str);
-	extern void LogEmergency(const char *fmt, ...);
+	extern void LogEmergency(const char *fmt, ...) __attribute__ ((format (printf, 1, 2)));
 	extern void LogEmergency(const std::string &str);
 
 	/* Ask for log rotation */
