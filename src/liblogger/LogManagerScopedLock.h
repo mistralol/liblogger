b980c74df2a7c3aa7ecedb8c2e6c044e00d812a1
James Stevenson <james.stevenson@calnexsol.com>
2019-10-16 15:34:19 +0100

    Fix performance issues

--- src/liblogger/LogManagerScopedLock.h
+++ src/liblogger/LogManagerScopedLock.h
@@ -5,10 +5,20 @@ namespace liblogger
 	class LogManagerScopedLock
 	{
 		public:
-			LogManagerScopedLock();
-			~LogManagerScopedLock();
+			LogManagerScopedLock() {
+				LogManager::Lock();
+				m_locked = true;
+			}
+			~LogManagerScopedLock() {
+				if (m_locked == true) {
+					LogManager::Unlock();
+				}
+			}
 
-			void Unlock();
+			void Unlock() {
+				m_locked = false;
+				LogManager::Unlock();
+			}
 		private:
 			bool m_locked;
 	};
